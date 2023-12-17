5d74af12d215e4cb3cad38d9e961ed61f422b63e
Diaa Ahmed <diaalion06@gmail.com>
2023-12-16 23:13:34 -0800

    DDL table creation

---
+++ car_rentalDDL.sql
@@ -0,0 +1,52 @@
+/*this code is used once then coment it*/
+create database Car_rental;
+/*-----------------------------*/
+use Car_rental;
+
+/*this code writen by Mohamed Mohab*/
+create table office (
+    id int PRIMARY KEY,
+    name varchar(225),
+    country varchar(225),
+    phone_number int
+);
+
+create table car (
+    color varchar(225),
+    status varchar(225),
+    plate_id int PRIMARY KEY,
+    mode varchar(255),
+    year int,
+    price int,
+    mileage int,
+    office_id int
+);
+
+
+create table reservation (
+    start_date date,
+    end_date date,
+    total_price int,
+    cutomer_id int,
+    plate_id int
+);
+
+create table customer (
+    id int,
+    email varchar(225),
+    name varchar(225),
+    password varchar(225),
+    phone_number int,
+    car_id int
+);
+create table address (
+    id int,
+    country varchar(225),
+    governorate varchar(225)
+);
+
+ALTER TABLE customer ADD PRIMARY KEY(id,email);
+ALTER table address ADD PRIMARY KEY(id, country);
+/*------------------------------------------------*/
+
+/*this code writen by Mahmoud reda*/
