9f72b3409517c1a752a40d37326d192bca0fe7af
Rinzler <divosilver.144@gmail.com>
2023-12-21 00:03:44 +0200

    made the register functionality

--- carbook-master/register.php
+++ carbook-master/register.php
@@ -1,3 +1,41 @@
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+    session_start();
+    $servername = "localhost";
+    $username = "root";
+    $pass = "";
+    $dbname = "car_rental";
+
+    $conn = mysqli_connect($servername, $username, $pass, $dbname);
+    if(! $conn)
+    {
+      die("failed to connect!");
+    }
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $password = $_POST["password"];
+    $confirm_password = $_POST["confirm_password"];
+    $duplicate = mysqli_query($conn, "SELECT * FROM CUSTOMER WHERE CUSTOMER.email = '$email' ");
+
+    if(mysqli_num_rows($duplicate) > 0){
+        echo "<script>alert(\"this email is already used\");</script>";
+    }else{
+        if($password == $confirm_password){
+            $query = "INSERT INTO CUSTOMER (name, email, phone_number, password) VALUES('$name', '$email', '$phone', '$password')";
+            mysqli_query($conn, $query);
+            echo"<script>alert(\"registeration succesfull\");</script>";
+        }
+        else
+        {
+          echo"<script>alert(\"assword confirmation failed\");</script>";
+        }
+    }
+
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,27 +68,27 @@
 <body>
   
   <div class="container">
-    <form action="#" class="request-form ftco-animate bg-primary fadeInUp ftco-animated">
+    <form action="#" method="post" class="request-form ftco-animate bg-primary fadeInUp ftco-animated">
       <h2>Welcome to AutoRent</h2>
       <div class="form-group">
         <label for="" class="label">User name</label>
-        <input type="text" class="form-control" placeholder="Enter Your name">
+        <input type="text" class="form-control" placeholder="Enter Your name" name="name">
       </div>
       <div class="form-group">
         <label for="" class="label">phone</label>
-        <input type="text" class="form-control" placeholder="Enter Your phone number">
+        <input type="text" class="form-control" placeholder="Enter Your phone number" name="phone">
       </div>
       <div class="form-group">
         <label for="" class="label">Email</label>
-        <input type="email" class="form-control" placeholder="Enter Your Email">
+        <input type="email" class="form-control" placeholder="Enter Your Email" name="email">
       </div>
       <div class="form-group">
         <label for="" class="label">Password</label>
-        <input type="password" class="form-control" placeholder="Enter Password">
+        <input type="password" class="form-control" placeholder="Enter Password" name="password">
       </div>
       <div class="form-group">
         <label for="" class="label">confirm Password</label>
-        <input type="password" class="form-control" placeholder="confirm your password">
+        <input type="password" class="form-control" placeholder="confirm your password" name="confirm_password">
       </div>
       <div class="form-group">
         <input type="submit" value="Register" class="btn btn-secondary py-3 px-4">
---
+++ carbook-master/sql/car_rentalDDL.sql
@@ -0,0 +1,59 @@
+create database Car_rental;
+
+use Car_rental;
+
+
+create table office (
+    id int PRIMARY KEY,
+    name varchar(225),
+    country varchar(225),
+    phone_number int
+);
+
+create table car (
+    plate_id int PRIMARY KEY,
+    color varchar(225),
+    status varchar(225),
+    mode varchar(255),
+    image varchar(255),
+    year int,
+    price int,
+    mileage int,
+    office_id int,
+    FOREIGN KEY(office_id) REFERENCES office(id)
+);
+
+create table customer (
+    id int,
+    email varchar(225),
+    name varchar(225),
+    password varchar(225),
+    phone_number int,
+    car_id int,
+    PRIMARY KEY(id, email),
+    FOREIGN KEY(car_id) REFERENCES car(plate_id)
+);
+
+create table reservation (
+    start_date date,
+    end_date date,
+    total_price int,
+    customer_id int,
+    plate_id int,
+    PRIMARY KEY(customer_id, plate_id),
+    FOREIGN KEY(plate_id) REFERENCES car(plate_id),
+    FOREIGN KEY(customer_id) REFERENCES customer(id)
+);
+
+create table address (
+    id int,
+    country varchar(225),
+    governorate varchar(225),
+    PRIMARY KEY(id, country),
+    FOREIGN KEY(id) REFERENCES customer(id)
+);
+
+/*
+    table creation, primary keys by Mohammed Mohab and Diaa Ahmad
+    foreign keys by Youssef Mahmoud
+*/
